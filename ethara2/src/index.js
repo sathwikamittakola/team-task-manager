a5bd28e510ab54e3936cebe7050895833273f002
sathwikamittakola <sathwikamittakola8@gmail.com>
2026-04-30 20:41:52 +0530

    Fix duplicate app declaration in index.js
    Removed duplicate declaration of the 'app' variable.

--- ethara2/src/index.js
+++ ethara2/src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/errorHandler');
-
+const app=express();
 
 // Connect to database
 connectDB();
