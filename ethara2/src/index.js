ffa7d78ddb55fe45b269c80d7ef662e1c5ea1e5f
sathwika <sathwika>
2026-05-07 16:45:57 +0530

    Add home route

--- ethara2/src/index.js
+++ ethara2/src/index.js
@@ -48,3 +48,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.send('Backend is running successfully');
+});
